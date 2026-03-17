5143f94a276ac370741d61a061bbc2b46f4ff5f0
Michée Kosga <micheekosga@gmail.com>
2026-03-17 07:24:16 +0100

    added visuals

---
+++ tiled/grass_cliff_spring.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="grass_cliff_spring" tilewidth="16" tileheight="16" tilecount="480" columns="24">
+ <image source="../assets/tilesources/tilesets/Tileset Grass Cliff Tileset Spring.png" width="384" height="320"/>
+</tileset>
--- world/entities/villager/villager.go
+++ world/entities/villager/villager.go
@@ -1,15 +1,41 @@
 components {
   id: "villager"
   component: "/world/entities/villager/villager.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"idle-front\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/tilesources/villager/alex/alex-idle.tilesource\"\n"
+  "}\n"
+  ""
+}
+embedded_components {
+  id: "collision"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"default\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "    id: \"box\"\n"
+  "  }\n"
+  "  data: 2.0\n"
+  "  data: 9.333334\n"
+  "  data: 10.0\n"
+  "}\n"
+  ""
 }
